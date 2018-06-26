2405abbb9bf3ed236a7ca8ca1812c9d5ed89f174
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-26 15:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function appendKitten(name) {
+ var morecats = [...kittens,"Broom"];
+  return morecats;
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function prependKitten(name) {
+  var morecats = ['Arnold', ...kittens];
+  return morecats;
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function removeLastKitten() {
+ var lesscats = kittens.slice[2]
+  return lesscats
+}
